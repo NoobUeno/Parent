82f96bf50e4d14682fc9029f58beab2d937dbc8e
NoobUeno <uenonao@outlook.com>
2023-06-29 17:59:10 +0800

    update 17点59分

--- demo/src/main/java/com/yao/demo/test.java
+++ demo/src/main/java/com/yao/demo/test.java
@@ -1,11 +1,17 @@
 package com.yao.demo;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson2.JSON;
+import com.yao.entity.Capture;
 import com.yao.entity.User;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import sun.misc.BASE64Decoder;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 /**
  * @Entity: testController
@@ -16,7 +22,11 @@ import java.util.List;
  **/
 public class test {
 
-    public static void main(String[]Args){
+    public static void main(String[] args) throws IOException {
+        new test().f2();
+    }
+
+    public void f1(){
         List<User> list = new ArrayList<User>();
         for (int i = 0; i <= 10; i++) {
             User user = new User();
@@ -37,4 +47,52 @@ public class test {
         System.out.println(date1);
     }
 
+
+    public void f2() throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+
+        File file = new File("C:\\Users\\Ueno\\Desktop\\a.txt");
+        FileInputStream inputStream = new FileInputStream(file);
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String string = null;
+
+        byte[] bytes = new byte[1024];
+
+        int len = 0;
+
+        while ((string = br.readLine()) != null){
+            sb.append(string);
+        }
+
+
+
+        Capture capture = JSON.parseObject(sb.toString(), Capture.class);
+
+
+        String imgStr = capture.getCaptureFaceBase64();
+        imgStr = imgStr.replaceAll("data:image/jpg;base64,","");
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] buffer = decoder.decodeBuffer(imgStr);
+        MockMultipartFile jpg = new MockMultipartFile(UUID.randomUUID() + ".jpg", buffer);
+
+        File out = new File("b.jpg");
+        FileOutputStream outputStream = new FileOutputStream(out);
+        BufferedOutputStream stream = new BufferedOutputStream(outputStream);
+        stream.write(buffer);
+
+    }
+
+    public String baseSwitch(String captureJsonString) throws IOException {
+
+        Capture capture = JSON.parseObject(captureJsonString, Capture.class);
+
+        String imgStr = capture.getCaptureFaceBase64();
+        imgStr = imgStr.replaceAll("data:image/jpg;base64,","");
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] buffer = decoder.decodeBuffer(imgStr);
+        MockMultipartFile jpg = new MockMultipartFile(UUID.randomUUID() + ".jpg", buffer);
+        return null;
+    }
 }
